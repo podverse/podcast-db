77dc6a4b8f56f9533ff54ac4d168324f1e8ca4fc
Mitch Downey <mitchldowney@gmail.com>
2017-01-09 20:10:23 -0600

    Use default postgres port if running a CircleCI test

--- src/config.js
+++ src/config.js
@@ -1,9 +1,10 @@
 let config = {};
+const isCi = require('is-ci');
 
-if (process.env.NODE_ENV === 'test_db') {
+if (process.env.NODE_ENV === 'test_db' && !isCi) {
   config.postgresUri = 'postgres://postgres:password@127.0.0.1:4443/postgres';
 } else {
-  config.postgresUri = 'postgres://postgres:password@127.0.0.1:5432/podverse';
+  config.postgresUri = 'postgres://postgres:password@127.0.0.1:5432/postgres';
 }
 
 module.exports = config;
