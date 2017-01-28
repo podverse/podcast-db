2fed7fa13f4247985ea3c442d4e6e864dbef7757
Mitch Downey <mitchldowney@gmail.com>
2017-01-28 14:02:06 -0600

    Update config to use process.env vars

--- src/config.js
+++ src/config.js
@@ -3,10 +3,12 @@ const isCi = require('is-ci');
 
 if (process.env.NODE_ENV === 'test_db' && !isCi) {
   config.postgresUri = 'postgres://postgres:password@127.0.0.1:4443/postgres';
+} else if (isCi) {
+  config.posgresUri = 'postgres://postgres:password@127.0.0.1:5432/postgres';
 } else {
-  config.postgresUri = 'postgres://postgres:password@127.0.0.1:5432/postgres';
+  config.postgresUri = process.env.postgresUri || 'postgres://postgres:password@127.0.0.1:5432/postgres';
 }
 
-config.queueUrl = '';
+config.queueUrl = process.env.queueUrl || '';
 
 module.exports = config;
