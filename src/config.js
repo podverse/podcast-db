1f34aa15c53490458307032626761dc1046c4b40
Mitch Downey <mitchldowney@gmail.com>
2017-01-28 14:10:14 -0600

    Fix typo

--- src/config.js
+++ src/config.js
@@ -4,7 +4,7 @@ const isCi = require('is-ci');
 if (process.env.NODE_ENV === 'test_db' && !isCi) {
   config.postgresUri = 'postgres://postgres:password@127.0.0.1:4443/postgres';
 } else if (isCi) {
-  config.posgresUri = 'postgres://postgres:password@127.0.0.1:5432/postgres';
+  config.postgresUri = 'postgres://postgres:password@127.0.0.1:5432/postgres';
 } else {
   config.postgresUri = process.env.postgresUri || 'postgres://postgres:password@127.0.0.1:5432/postgres';
 }
