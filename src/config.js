a2e65a7b4c4ed3273a869a0597e462648f1eb1f5
Mitch Downey <mitchldowney@gmail.com>
2017-01-31 20:42:15 -0600

    Remove extra postgresUri config

--- src/config.js
+++ src/config.js
@@ -1,13 +1,7 @@
 let config = {};
 const isCi = require('is-ci');
 
-if (process.env.NODE_ENV === 'test_db' && !isCi) {
-  config.postgresUri = 'postgres://postgres:password@127.0.0.1:4443/postgres';
-} else if (isCi) {
-  config.postgresUri = 'postgres://postgres:password@127.0.0.1:5432/postgres';
-} else {
-  config.postgresUri = process.env.postgresUri || 'postgres://postgres:password@127.0.0.1:5432/postgres';
-}
+config.postgresUri = process.env.postgresUri || 'postgres://postgres:password@127.0.0.1:5432/postgres';
 
 config.queueUrl = process.env.queueUrl || '';
 
