993f69a13bf924bd6aa950ea9ffd8f42c6920224
Mitch Downey <mitchldowney@gmail.com>
2017-02-19 21:28:33 -0600

    Rename migration file

--- src/repositories/sequelize/migrations/20170219211000-initial-migration.js
+++ src/repositories/sequelize/migrations/20170219211000-initial-migration.js
@@ -2,13 +2,13 @@ const {readFileSync} = require('fs');
 
 module.exports = {
   up: function (queryInterface, Sequelize) {
-    
+
     let initialSql = readFileSync(__dirname + '/initial.sql', 'utf-8');
 
-    queryInterface.sequelize.query(initialSql, { 
+    queryInterface.sequelize.query(initialSql, {
       raw: true, type: Sequelize.QueryTypes.RAW
     });
-  
+
   },
 
   down: function (queryInterface) {
