b6bafbff35f430b8dcdbef6448b003de2ba390fc
Mitch Downey <mitchldowney@gmail.com>
2017-02-28 01:06:38 -0600

    Add Art of Charm title override

--- src/custom-overrides/podcastOverride.js
+++ src/custom-overrides/podcastOverride.js
@@ -5,6 +5,10 @@ function podcastOverride(podcast) {
     podcast.imageURL = 'http://is4.mzstatic.com/image/thumb/Music6/v4/7a/49/7f/7a497f92-4e9e-9d8f-2bda-841833894a34/source/600x600bb.jpg';
   }
 
+  if (podcast.xmlurl === 'http://theartofcharmpodcast.theartofcharm.libsynpro.com/rss') {
+      podcast.title = 'The Art of Charm';
+  }
+
   return podcast;
 }
 
