0de2285980f83780e25f1ee6e49e8794b5adb813
Mitch Downey <mitchldowney@gmail.com>
2018-01-01 22:00:50 -0600

    Add basic dead letter handling

--- src/main.js
+++ src/main.js
@@ -11,10 +11,28 @@ function addFeedUrlsToParsingQueue (params = {}) {
 function parseNextFeed () {
   parseNextFeedFromQueue()
     .then(arr => {
-      return parseFeed(arr[0], arr[1]) // feedUrl, params
-        .then(() => {
-          deleteSQSMessage(arr[2]); // receiptHandle
-        })
+
+      if (arr && arr.length > 0) {
+
+        console.log('Start parsing:', arr[0]);
+
+        return parseFeed(arr[0], arr[1]) // feedUrl, params
+          .then(() => {
+            deleteSQSMessage(arr[2]); // receiptHandle
+            parseNextFeed();
+            return;
+          })
+          .catch(e => {
+            // Message should eventually timeout in SQS queue and go to the
+            // dead letter queue.
+            console.log(e);
+            parseNextFeed();
+            return;
+          })
+      } else {
+        return
+      }
+
     })
     .catch(err => {
       console.log(err);
