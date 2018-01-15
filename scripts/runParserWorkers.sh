af69158cf5c3486aad4a3e8dffb6574fdaaf4dec
Mitch Downey <mitchldowney@gmail.com>
2018-01-14 23:26:35 -0600

    Update runParserWorkers to fix bug with using the $i variable in for loop

--- scripts/runParserWorkers.sh
+++ scripts/runParserWorkers.sh
@@ -1,1 +1,3 @@
-for i in {1..10}; do docker run -d --rm -e AWS_ACCESS_KEY_ID='' -e AWS_SECRET_ACCESS_KEY='' -e queueUrl='' -e postgresUri='' --name podcast_db_dev_$i podcast_db_dev /tmp/scripts/parseNextFeed.js; done
+for ((i = 1; i <= 10; i++))
+  do docker run -d --rm -e AWS_ACCESS_KEY_ID='' -e AWS_SECRET_ACCESS_KEY='' -e queueUrl='' -e postgresUri='' --name podcast_db_dev_$i podcast_db_dev /tmp/scripts/parseNextFeed.js
+done
