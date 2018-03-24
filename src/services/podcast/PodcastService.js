f38feb07bfae09f03fc4c734aa89b0946ef3dfdb
Mitch Downey <mitchldowney@gmail.com>
2018-03-24 15:22:45 -0500

    Include duration with Episodes from Podcast retrieval method

--- src/services/podcast/PodcastService.js
+++ src/services/podcast/PodcastService.js
@@ -24,6 +24,7 @@ class PodcastService extends SequelizeService {
         include: [{
           model: Episode,
           attributes: ['id', 'title', 'mediaUrl', 'pubDate', 'summary', 'isPublic']
+          attributes: ['id', 'title', 'mediaUrl', 'pubDate', 'summary', 'isPublic', 'duration'],
         }]
       }
     }
