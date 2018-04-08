c6ce3a6536f02a677a0f0fa15d1fca215dac4eee
Mitch Downey <mitchldowney@gmail.com>
2018-04-08 10:43:55 -0500

    Stop and remove parser workers before starting a new worker with the same name

--- scripts/runParserWorkers.sh
+++ scripts/runParserWorkers.sh
@@ -1,3 +1,5 @@
 for ((i = 1; i <= 10; i++))
-  do docker run -d --rm -e AWS_ACCESS_KEY_ID='' -e AWS_SECRET_ACCESS_KEY='' -e queueUrl='' -e postgresUri='' --name podcast_db_dev_$i podcast_db_dev /tmp/scripts/parseNextFeed.js
+  do docker stop feed_parser_worker$i;
+  docker rm feed_parser_worker$i;
+  docker run -d --rm -e AWS_ACCESS_KEY_ID='' -e AWS_SECRET_ACCESS_KEY='' -e queueUrl='' -e postgresUri='' --name podcast_db_dev_$i podcast_db_dev /tmp/scripts/parseNextFeed.js
 done
