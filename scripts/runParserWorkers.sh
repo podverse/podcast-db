82e550995d42a756f8dd64f5041d1ebc64b3837e
Mitch Downey <mitchldowney@gmail.com>
2018-04-08 11:40:03 -0500

    Reduce the number of parser workers to 8

--- scripts/runParserWorkers.sh
+++ scripts/runParserWorkers.sh
@@ -1,4 +1,4 @@
-for ((i = 1; i <= 10; i++))
+for ((i = 1; i <= 8; i++))
   do docker stop feed_parser_worker$i;
   docker rm feed_parser_worker$i;
   docker run -d --rm -e AWS_ACCESS_KEY_ID='' -e AWS_SECRET_ACCESS_KEY='' -e queueUrl='' -e postgresUri='' --name feed_parser_worker$i podcast_db /tmp/scripts/parseNextFeed.js
