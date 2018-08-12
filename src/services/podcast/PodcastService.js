ceb99ef32f7dcc291505fc86921c2a48c5eb8747
Mitch Downey <mitchldowney@gmail.com>
2018-08-12 18:21:50 -0500

    Enable retrieving only public episodes with podcast get method

--- src/services/podcast/PodcastService.js
+++ src/services/podcast/PodcastService.js
@@ -1,22 +1,22 @@
 const
-    sqlEngine = require('../../repositories/sequelize/engineInstance.js'),
-    modelFactory = require('../../repositories/sequelize/models'),
-    SequelizeService = require('feathers-sequelize').Service,
-    errors = require('feathers-errors');
+  sqlEngine = require('../../repositories/sequelize/engineInstance.js'),
+  modelFactory = require('../../repositories/sequelize/models'),
+  SequelizeService = require('feathers-sequelize').Service,
+  errors = require('feathers-errors');
 
 const Models = modelFactory(sqlEngine);
 
 class PodcastService extends SequelizeService {
 
-  constructor () {
+  constructor() {
     super({
       Model: Models.Podcast
     });
     this.Models = Models;
   }
 
-  get (id, params={}) {
-    const {Episode} = this.Models;
+  get(id, params = {}) {
+    const { Episode } = this.Models;
 
     if (!params.excludeEpisodes) {
       params.sequelize = {
@@ -25,14 +25,18 @@ class PodcastService extends SequelizeService {
           attributes: ['id', 'title', 'mediaUrl', 'pubDate', 'summary', 'isPublic', 'duration']
         }]
       }
+
+      if (params.isPublic) {
+        params.sequelize.include[0].where = { isPublic: true };
+      }
     }
 
     return super.get(id, params);
 
   }
 
-  find (params={}) {
-    const {FeedUrl} = this.Models;
+  find(params = {}) {
+    const { FeedUrl } = this.Models;
 
     // Fuzzy match search for podcasts by title
     if (typeof params.query !== 'undefined' && typeof params.query.title !== 'undefined' && params.query.title.length > 0) {
@@ -57,8 +61,8 @@ class PodcastService extends SequelizeService {
     }
   }
 
-  findOrCreatePodcastFromParsing (parsedPodcast, podcastId) {
-    
+  findOrCreatePodcastFromParsing(parsedPodcast, podcastId) {
+
     let podcast = parsedPodcast;
 
     return this.Model.findOrCreate({
@@ -69,55 +73,55 @@ class PodcastService extends SequelizeService {
         title: podcast.title
       }
     })
-    .then((podcastArray) => {
-      podcast.id = podcastArray[0].id;
+      .then((podcastArray) => {
+        podcast.id = podcastArray[0].id;
 
-      let image = podcast.image || {};
+        let image = podcast.image || {};
 
-      return this.Model.upsert({
-        id: podcast.id,
-        imageUrl: image.url,
-        summary: podcast.description,
-        title: podcast.title,
-        author: podcast.author,
-        lastBuildDate: podcast.date,
-        lastPubDate: podcast.pubdate,
-        lastEpisodeTitle: podcast.lastEpisodeTitle,
-        totalAvailableEpisodes: podcast.totalAvailableEpisodes,
-        categories: podcast.categories
+        return this.Model.upsert({
+          id: podcast.id,
+          imageUrl: image.url,
+          summary: podcast.description,
+          title: podcast.title,
+          author: podcast.author,
+          lastBuildDate: podcast.date,
+          lastPubDate: podcast.pubdate,
+          lastEpisodeTitle: podcast.lastEpisodeTitle,
+          totalAvailableEpisodes: podcast.totalAvailableEpisodes,
+          categories: podcast.categories
+        })
+          .then(() => {
+            return podcast.id;
+          });
       })
-      .then(() => {
-        return podcast.id;
-      });
-    })
-    .catch(err => {
-      console.log(err);
-      console.log(podcast.title);
-      console.log(podcast.xmlurl);
-    })
+      .catch(err => {
+        console.log(err);
+        console.log(podcast.title);
+        console.log(podcast.xmlurl);
+      })
   }
 
   findPodcastByFeedUrl(url) {
-    const {FeedUrl} = this.Models;
+    const { FeedUrl } = this.Models;
 
     return FeedUrl.find({
       where: {
         url: url
       }
     })
-    .then(feedUrl => {
+      .then(feedUrl => {
 
-      if (feedUrl && feedUrl.podcastId) {
+        if (feedUrl && feedUrl.podcastId) {
 
-        return this.get(feedUrl.podcastId)
-        .then(podcast => {
-          return podcast
-        })
+          return this.get(feedUrl.podcastId)
+            .then(podcast => {
+              return podcast
+            })
 
-      } else {
-        throw new errors.GeneralError('FeedUrl not found');
-      }
-    })
+        } else {
+          throw new errors.GeneralError('FeedUrl not found');
+        }
+      })
   }
 
 }
